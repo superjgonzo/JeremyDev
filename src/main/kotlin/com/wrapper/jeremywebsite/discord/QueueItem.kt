6169ac7b3f79b1ecb915c73c768e90d12be4e8c2
jeremy <jgonzo12.jg@gmail.com>
2023-12-22 13:01:50 -0600

    renaming discord package and adding new app.yaml files for app engine

--- src/main/kotlin/com/wrapper/jeremywebsite/discord/QueueItem.kt
+++
@@ -1,9 +0,0 @@
-package com.wrapper.jeremywebsite.discord
-
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack
-import org.javacord.api.entity.user.User
-
-data class QueueItem(
-  val track: AudioTrack,
-  val queuedBy: User
-)
