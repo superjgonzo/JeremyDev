6e04641ea07a5f7c5ad463e328b15c6cdfc04b51
jeremy <jgonzo12.jg@gmail.com>
2023-12-22 13:13:09 -0600

    renaming discord package and adding new app.yaml files for app engine

---
+++ src/main/kotlin/com/wrapper/jeremywebsite/discordbot/LavaPlayerAudioSource.kt
@@ -0,0 +1,32 @@
+package com.wrapper.jeremywebsite.discordbot
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer
+import com.sedmelluq.discord.lavaplayer.track.playback.AudioFrame
+import org.javacord.api.DiscordApi
+import org.javacord.api.audio.AudioSource
+import org.javacord.api.audio.AudioSourceBase
+
+
+/**
+ * Creates a new lavaplayer audio source.
+ *
+ * @param api A discord api instance.
+ * @param audioPlayer An audio player from Lavaplayer.
+ */
+class LavaPlayerAudioSource(api: DiscordApi?, private val audioPlayer: AudioPlayer) : AudioSourceBase(api) {
+
+  private var lastFrame: AudioFrame? = null
+
+  override fun getNextFrame(): ByteArray? {
+    return lastFrame?.let { applyTransformers(it.data) }
+  }
+
+  override fun hasFinished(): Boolean = false
+
+  override fun hasNextFrame(): Boolean {
+    lastFrame = audioPlayer.provide()
+    return lastFrame != null
+  }
+
+  override fun copy(): AudioSource = LavaPlayerAudioSource(api, audioPlayer)
+}
