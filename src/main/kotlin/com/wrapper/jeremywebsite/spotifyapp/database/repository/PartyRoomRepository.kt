13b1e4c5473a701ed42add114e15f3657bb25786
jeremy <jgonzo12.jg@gmail.com>
2024-01-02 22:10:49 -0600

    attempting to restructure directories

--- src/main/kotlin/com/wrapper/jeremywebsite/spotifyapp/database/repository/PartyRoomRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.wrapper.jeremywebsite.spotifyapp.database.repository
-
-import com.wrapper.jeremywebsite.spotifyapp.database.models.PartyRoom
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface PartyRoomRepository : JpaRepository<PartyRoom, String>
--- src/test/kotlin/com/wrapper/jeremywebsite/JeremyWebsiteApplicationTests.kt
+++
@@ -1,9 +0,0 @@
-package com.wrapper.jeremywebsite
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class JeremyWebsiteApplicationTests {
-
-}
